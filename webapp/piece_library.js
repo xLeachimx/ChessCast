24b743a5d6bc2676c93dfa2dd8849b3027f17e67
xLeachimx <michael.huelsman@gmail.com>
2015-11-14 11:20:51 -0500

    forgot to export point

--- webapp/piece_library.js
+++ webapp/piece_library.js
@@ -4,6 +4,7 @@ var bishop = require('pawn.js');
 var knight = require('pawn.js');
 var queen = require('pawn.js');
 var king = require('pawn.js');
+var base = require('piece.js');
 
 module.exports = {
   Pawn: pawn,
@@ -11,5 +12,6 @@ module.exports = {
   Bishop: bishop,
   Knight: knight,
   Queen: queen,
-  King: king
+  King: king,
+  Point: base.Point
 };
