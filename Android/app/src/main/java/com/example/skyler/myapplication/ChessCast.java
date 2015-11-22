bdfd5796847c3a7c47a3790391bbcd305e51a143
SkylerMoran <sxmoran16@transy.edu>
2015-11-22 15:45:29 -0500

    Chromecast Receiver
    Altered app icon and name.
    Started to add functions related to ChromeCast connectivity

--- Android/app/src/main/java/com/example/skyler/myapplication/ChessCast.java
+++ Android/app/src/main/java/com/example/skyler/myapplication/ChessCast.java
@@ -4,26 +4,107 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
+import android.media.browse.MediaBrowser;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.MediaRouteButton;
+import android.support.v7.media.MediaRouteSelector;
+import android.support.v7.media.MediaRouter;
+import android.support.v7.media.MediaRouter.RouteInfo;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
+
+import com.google.android.gms.cast.Cast;
+import com.google.android.gms.cast.CastDevice;
+import com.google.android.gms.cast.CastMediaControlIntent;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+
 public class ChessCast extends AppCompatActivity {
+    MediaRouter mMediaRouter;
+    private MediaRouteButton mMediaRouteButton;
+
+    private MediaRouteSelector mMediaRouteSelector;
+    private CastDevice mSelectedDevice;
+    private MediaRouter.Callback mMediaRouterCallback;
+    TextView mConnectionStatus;
+
+    private GoogleApiClient mApiClient;
+    private Cast.Listener mCastListener;
+    private GoogleApiClient.ConnectionCallbacks mConnectionCallbacks;
+    private GoogleApiClient.OnConnectionFailedListener mConnectionFailedListener;
+    private boolean mApplicationStarted;
+    private boolean mWaitingForReconnect;
+    private String mSessionId;
+
+    String APP_ID = "F6D3E50B";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chess_cast);
+        //Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+       // mConnectionStatus = (TextView)findViewById(R.id.text_connection);
+        mMediaRouteButton= (MediaRouteButton)findViewById(R.id.media_route_button);
+
+        initCast();
+    }
+    private void initCast(){
+        mMediaRouter = MediaRouter.getInstance(getApplicationContext());
+        mMediaRouteSelector = new MediaRouteSelector.Builder()
+                .addControlCategory(CastMediaControlIntent.categoryForCast(APP_ID))
+                .build();
+
+        mMediaRouteButton.setRouteSelector(mMediaRouteSelector);
+        mMediaRouterCallback = new MyMediaRouterCallback();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mMediaRouter.addCallback(mMediaRouteSelector, mMediaRouterCallback,
+                MediaRouter.CALLBACK_FLAG_REQUEST_DISCOVERY);
+    }
 
-    /*public void sendMessage(View v) {
-        Button button=(Button) v;
-        ((Button) v).setText("Clicked");
+    @Override
+    protected void onStop() {
+        mMediaRouter.removeCallback(mMediaRouterCallback);
+        super.onStop();
+    }
+
+    private class MyMediaRouterCallback extends MediaRouter.Callback {
+
+        @Override
+        public void onRouteSelected(MediaRouter router, MediaRouter.RouteInfo info) {
+            mSelectedDevice = CastDevice.getFromBundle(info.getExtras());
+            mConnectionStatus.setText("Connected to: " + mSelectedDevice.getFriendlyName());
+        }
+
+        @Override
+        public void onRouteUnselected(MediaRouter router, MediaRouter.RouteInfo info) {
+            mSelectedDevice = null;
+            mConnectionStatus.setText("Not Connected");
+        }
+
+    }
+/*
+    private void launchReceiver(){
+        mCastListener = new Cast.Listener(){
+            @Override
+            public void onApplicationDisconnected(int errorCode){
+                Log.d(TAG, "application has stopped");
+                teardown(true);
+            }
+        };
+        mConnectionCallbacks = new GoogleApiClient.ConnectionCallbacks();
+        mConnectionFailedListener = new GoogleApiClient.OnConnectionFailedListener();
     }*/
-
-
-
 }
+
---
+++ Android/app/src/main/java/com/example/skyler/myapplication/Setting.java
@@ -0,0 +1,14 @@
+package com.example.skyler.myapplication;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Setting extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+
+}
